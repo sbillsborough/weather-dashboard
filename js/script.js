d2d3797eefc512176c1de55813861ed3793e75de
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2022-12-21 17:09:03 +0000

    added getWeatherData function and conditional that checks if the city variable is true

--- js/script.js
+++ js/script.js
@@ -1,2 +1,38 @@
 var currentDay = moment().format("(DD/MM/YYYY)");
 $("#todayDate").text(currentDay);
+
+var searchInput = $("#search-input");
+var searchForm = $("#search-form");
+var searchButton = $("#search-button");
+
+var apiKey = "b4d39ba071aaf22dfaae85c01257a991";
+var city = "Prague";
+
+var iconUrl = "https://openweathermap.org/img/w/";
+
+function getWeatherData() {
+  if (city) {
+    $.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    ).then(function (currentData) {
+      console.log(
+        `
+      Temp: ${Math.round(currentData.main.temp)}
+      Wind: ${currentData.wind.speed}
+      Humidity: ${currentData.main.humidity}%
+      Icon URL: ${iconUrl + currentData.weather[0].icon + ".png"}
+    `
+      );
+
+      $.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+      ).then(function (forecastData) {
+        console.log(forecastData);
+      });
+    });
+  } else {
+    return;
+  }
+}
+
+getWeatherData();
